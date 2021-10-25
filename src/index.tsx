340fcbbe42b233cf929dd6fa913f5794db783b46
Esra Cebi <esracebi.81@gmail.com>
2021-10-25 18:49:45 +0300

    redux kurulum

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import 'antd/dist/antd.css';
+import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import "antd/dist/antd.css";
 
+import App from "./App";
+import rootReducer from "./store";
+import { Provider } from "react-redux";
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
 );
-
---
+++ src/types/user.ts
@@ -0,0 +1,30 @@
+import { type } from "os";
+
+export interface User {
+    message: string;
+    username: string;
+    email: string;
+    full_name: string;
+    token: string;
+}
+
+export interface UserState {
+    data: User;
+    loading: boolean;
+    error: string;
+}
+
+interface LOGIN_START {
+    type: "LOGIN_START";
+}
+
+interface LOGIN_SUCCESS {
+    type: "LOGIN_SUCCESS";
+    payload: User;
+
+}
+interface LOGIN_ERROR {
+    type: "LOGIN_ERROR";  
+}
+
+export type UserAction = LOGIN_START | LOGIN_SUCCESS | LOGIN_ERROR;

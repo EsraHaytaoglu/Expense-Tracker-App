1ebbe5c62c7a56a1846cb0d37a80df5269a893a8
Esra Cebi <esracebi.81@gmail.com>
2021-10-25 19:41:07 +0300

    register sayfası

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import React from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
+import { Route } from "react-router-dom";
+import SignUp from "./components/SignUp";
+
 function App() {
+  const { Header, Content, Footer } = Layout;
   return (
-    <div >
-      App
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
+          {new Array(15).fill(null).map((_, index) => {
+            const key = index + 1;
+            return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+          })}
+        </Menu>
+      </Header>
+      <Content style={{ padding: "50px 50px" }}>
+        <Route path="/register" component={SignUp} />
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Harcama Takip ©2021 Esra Çebi
+      </Footer>
+    </Layout>
   );
 }
 

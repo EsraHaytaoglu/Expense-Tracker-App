8758be35237d52d9acc006ab575e25202f73c42a
Esra Cebi <esracebi.81@gmail.com>
2021-10-27 15:23:02 +0300

    token hatası çözüldü cetagoriler alınıyor.

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,12 +1,14 @@
 import axios from "axios";
 
 export default () => {
-  const token = JSON.parse(localStorage.getItem("token")!);
+  const token = localStorage.getItem("token");
+
+  const token2 = token !== null ? JSON.parse(token) : [];
 
   return axios.create({
     baseURL: "https://expensetracker-be.herokuapp.com",
     headers: {
-      Authorization: token
+      Authorization: token2
     },
   });
 };
